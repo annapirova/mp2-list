aac37bccdd58bd1f208d7790c6f523dffdb8241e
Vladimir Loginov <morkovka1337@users.noreply.github.com>
2017-12-14 15:41:56 +0300

    done smth

--- include/list.h
+++ include/list.h
@@ -10,17 +10,29 @@ struct Node
 {
   DataType data;
   Node* next;
+  Node(DataType _data, Node* _next) {
+	  data = _data;
+	  next = _next;
+  }
   // здесь могут быть конструкторы, методы, деструктор Node
 };
 
 class List
 {
+	Node* head;
   // любые поля
 public:
   List() {};
   List(const List& list2);
   List& operator=(const List& list2);
-  ~List() {};
+  ~List() {
+	  Node* tmp = head;
+	  while (tmp!= NULL)
+	  {
+		  delete tmp;
+		  tmp = tmp->next;
+	  }
+  };
 
   void InserToHead(const DataType& d); // вставить элемент d первым
   void InserToTail(const DataType& d); // вставить элемент d последним
@@ -28,8 +40,8 @@ public:
   void Delete(const DataType& d); // удалить звено со значением data = d
   Node* Search(const DataType& d); // найти указатель на звено со значением data = d
   void Clean(); // удалить все звенья
-  int GetSize(); // узнать число звеньев в списке
-  Node* GetHead(); // получить указатель на первое звено списка
+  int GetSize() { int k = 0; Node* tmp = head;  while (tmp->next != NULL) k++; return k; }; // узнать число звеньев в списке
+  Node* GetHead() { return head; } // получить указатель на первое звено списка
 
   void Inverse(); // инвертировать список, т.е. звенья должны идти в обратном порядке
   List Merge(Node* node, const List& list2); // создать список3, добавив список2 в текущий список после указателя node  
--- src/list.cpp
+++ src/list.cpp
@@ -1,1 +1,61 @@
 #include "list.h"
+List::List(const List& list2)
+{
+	head = new Node(list2->head);
+}
+
+void List::InserToHead(const DataType& d)
+{
+	
+}
+
+void List::InserToTail(const DataType& d)
+{
+	Node* tmp;
+	tmp = head;
+	if (tmp == NULL)
+		head = new Node(data, NULL);
+	else 
+	{
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+	}
+	tmp->next = new Node(data, NULL);
+}
+
+void List::InsertAfter(Node* node, const DataType& d)
+{
+	Node* n = new Node(data, node->next);
+	node->next = n;
+}
+
+Node* List::Search(const DataType& d)
+{
+	Node* tmp = head;
+	while (tmp != NULL)
+	{
+		if (tmp->data == data)
+			return tmp;
+		else 
+			tmp = tmp->next;
+	}
+	return tmp;
+}
+
+void List::Delete(const DataType& d)
+{
+	Node* prev, *tmp;
+	tmp = head; prev = NULL;
+	while ((tmp != NULL) && (tmp->data != d))
+	{
+		prev = tmp;
+		tmp = tmp->next;
+	}
+	prev->next = tmp->next;
+	delete tmp;
+}
+
+void List::Inverse()
+{
+	Node* tmp = head;
+}
