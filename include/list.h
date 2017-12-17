78cb7c63c566ffccfecfa0e39b93ee9cf7e8e71d
Vladimir Repin <mezotaken@yandex.ru>
2017-12-17 14:48:58 +0300

    Готово

--- include/list.h
+++ include/list.h
@@ -8,13 +8,14 @@ typedef int DataType;
 
 struct Node
 {
-  DataType data;
-  Node* next;
-  // здесь могут быть конструкторы, методы, деструктор Node
+	DataType data;
+	Node* next;
+	// здесь могут быть конструкторы, методы, деструктор Node
 
-  Node(DataType d = 0, Node* n = NULL);
-  Node(const Node& node2);
-  bool operator==(const Node& node2) const;
+	Node(DataType d = 0, Node* n = NULL) { data = d; next = n; }
+	Node(const Node& node2) { data = node2.data; next = NULL; }
+	bool operator==(const Node& node2) const { return (data == node2.data && next == node2.next); }
+	bool operator!=(const Node& node2) const { return !(*this == node2); }
 };
 
 class List
@@ -22,7 +23,7 @@ class List
   // любые поля
   Node* head;
 public:
-  List();
+  List() { head = NULL; }
   List(const List& list2);
   List& operator=(const List& list2);
   ~List();
@@ -33,8 +34,8 @@ public:
   void Delete(const DataType& d); // удалить звено со значением data = d
   Node* Search(const DataType& d); // найти указатель на звено со значением data = d
   void Clean(); // удалить все звенья
-  int GetSize(); // узнать число звеньев в списке
-  Node* GetHead(); // получить указатель на первое звено списка
+  int GetSize() const; // узнать число звеньев в списке
+  Node* GetHead() { return head; } // получить указатель на первое звено списка
 
   void Inverse(); // инвертировать список, т.е. звенья должны идти в обратном порядке
   List Merge(Node* node, const List& list2); // создать список3, добавив список2 в текущий список после указателя node  
@@ -42,6 +43,7 @@ public:
 
   friend ostream& operator<<(ostream& os, const List& l);
   bool operator==(const List& list2) const; // списки равны, если элементы в них идут в одинаковом порядке
+  bool operator!=(const List& list2) const { return !(*this == list2); }
 };
 
 #endif
