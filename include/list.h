8fc54cdb1d31dca20fbd42252db6541c9d2f4ae0
Artem Shipilov <hugo.nieto@mail.ru>
2021-11-15 20:31:54 +0300

    realization

--- include/list.h
+++ include/list.h
@@ -14,7 +14,7 @@ struct Node
 
   Node(DataType d = 0, Node* n = NULL);
   Node(const Node& node2);
-  bool operator==(const Node& node2) const;
+  bool operator==(const Node& list2) const;
 };
 
 class myiterator
@@ -41,29 +41,46 @@ class List
 public:
   List();
 
-  bool isEmpty() { return (head == nullptr); }
+  bool isEmpty() 
+  { 
+	  return (head == nullptr); 
+  }
 //  List(const List& list2);
 //  List& operator=(const List& list2);
   ~List();
 
   void InsertToHead(const DataType& d); // вставить элемент d первым
-  DataType ViewHead() { return head->data;  }
+  DataType ViewHead() 
+  { 
+	  return head->data;  
+  }
   void InsertToTail(const DataType& d); // вставить элемент d последним
   void Clean(); // удалить все звенья  
   void Delete(const DataType& d); // удалить звено со значением data = d				
-  myiterator begin() { return myiterator(head); };
-				
+  myiterator begin() 
+  { 
+	  return myiterator(head); 
+  };
+
   //void InsertAfter(Node* node, const DataType& d); // вставить элемент d после звена node
-  //Node* Search(const DataType& d); // найти указатель на звено со значением data = d
+
+
+  myiterator Search(const DataType& d); // найти указатель на звено со значением data = d
+
+
   //int GetSize(); // узнать число звеньев в списке
   //Node* GetHead(); // получить указатель на первое звено списка
 
-  //void Inverse(); // инвертировать список, т.е. звенья должны идти в обратном порядке
+
+  void Inverse(); // инвертировать список, т.е. звенья должны идти в обратном порядке
+  //bool operator==(const Node& node2) const;
+
+
   //List Merge(Node* node, const List& list2); // создать список3, добавив список2 в текущий список после указателя node  
   //List Merge(const List& list2); // создать список3, добавив в конец текущего списка список2
 
   //friend ostream& operator<<(ostream& os, const List& l);
-  //bool operator==(const List& list2) const; // списки равны, если элементы в них идут в одинаковом порядке
+  bool operator==(const List& list2) const; // списки равны, если элементы в них идут в одинаковом порядке
 };
 
 #endif
